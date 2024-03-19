8ae7b4aa6d3054addb2011270ff3944b54576dd9
David Gonzalez <davidega12@gmail.com>
2024-03-18 18:11:52 -0600

    Enlazando con FireBase

--- src/components/pag_inicio/HomePageApp.tsx
+++ src/components/pag_inicio/HomePageApp.tsx
@@ -1,9 +1,27 @@
 
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { HomePageEdit } from './';
 import './HomePageApp.css'
+import { getFirebaseDoc } from '../../api/getFirebaseDoc/getFirebaseDoc';
 
 export const HomePageApp = () => {
+
+    //informacion de FireStore
+    const [titulo, setTitulo] = useState('');
+    const [descripcion, setDescripcion] = useState('');
+    const [image_url, setImageUrl] = useState('');
+
+    useEffect(() => {
+        (async() => {
+            const docSnap = await getFirebaseDoc('/Home/8Yl9xbZuRNFTUItTEKGU');
+
+            setTitulo(docSnap?.titulo);
+            setDescripcion(docSnap?.descripcion);
+            setImageUrl(docSnap?.image_url);
+            console.log(image_url);
+        })()
+    }, []);
+
     const [showEditPage, setShowEditPage ] = useState(false);
     const editRef = useRef<any>(null);
 
@@ -25,14 +43,14 @@ export const HomePageApp = () => {
         <div className="row text-start">
             {/* Contenido del lado izquierdo */}
             <div className="col-sm-5">
-                <h2 className='fw-bold color-title mb-3'>Titulo Titulo Titulo Titulo Titulo Titulo Titulo </h2>
-                <p>Aqui va la descripcion de la pantalla de inicio.... </p>
+                <h2 className='fw-bold color-title mb-3'>{ titulo }</h2>
+                <p>{ descripcion } </p>
             </div>
 
             {/* Contenido del lado derecho */}
             <div className="col-sm-7" ref={editRef}>
                 <img 
-                    src="/src/assets/turismo.jpg" 
+                    src={image_url} 
                     alt="Imagen de página de inicio,"
                     className='img-fluid rounded'
                     />
