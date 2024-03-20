1c01a2756d85a6c13009ad5e0e8f171e7d293148
David Gonzalez <davidega12@gmail.com>
2024-03-20 00:16:42 -0600

    Home Protection Logged out

--- src/pages/HomePage/HomePageApp.tsx
+++ src/pages/HomePage/HomePageApp.tsx
@@ -3,6 +3,9 @@ import { useState, useRef, useEffect } from 'react';
 import { HomePageEdit } from '../../pages/HomePage';
 import './HomePageApp.css'
 import { getFirebaseDoc } from '../../api/getFirebaseDoc/getFirebaseDoc';
+import { useNavigate } from 'react-router';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../redux/store';
 
 export const HomePageApp = () => {
 
@@ -11,53 +14,65 @@ export const HomePageApp = () => {
     const [descripcion, setDescripcion] = useState('');
     const [image_url, setImageUrl] = useState('');
 
+    const navigate = useNavigate();
+    // Redux Hooks & Access
+    const user = useSelector((state: RootState) => state.auth.user);
+    const loggedIn = useSelector((state: RootState) => state.auth.loggedIn);
+
     useEffect(() => {
-        (async() => {
+        (async () => {
             const docSnap = await getFirebaseDoc('/Home/8Yl9xbZuRNFTUItTEKGU');
 
             setTitulo(docSnap?.titulo);
             setDescripcion(docSnap?.descripcion);
             setImageUrl(docSnap?.image_url);
-            
+
         })()
     }, []);
 
-    const [showEditPage, setShowEditPage ] = useState(false);
+    // Redireccionar si está no logueado, y no hay usuario
+    useEffect(() => {
+        if (!loggedIn && !user) {
+            navigate("/");
+        }
+    }, [loggedIn, user, navigate]);
+
+    const [showEditPage, setShowEditPage] = useState(false);
     const editRef = useRef<any>(null);
 
 
     const handleEditClick = () => {
         setShowEditPage(true);
-        if (editRef.current){
-            editRef.current.scrollIntoView({ behavior: 'smooth'});
+        if (editRef.current) {
+            editRef.current.scrollIntoView({ behavior: 'smooth' });
         }
     };
 
-  return (
-    <div className="container mt-5">
-        {/* Boton de editar */}
-        <div className='text-start mb-5'>
-            <button type="button" className='btn btn-success' onClick={handleEditClick}>Editar</button>
-        </div>
-
-        <div className="row text-start">
-            {/* Contenido del lado izquierdo */}
-            <div className="col-sm-5">
-                <h2 className='fw-bold color-title mb-3'>{ titulo }</h2>
-                <p>{ descripcion } </p>
+    return (
+        <div className="container mt-5">
+            {/* Boton de editar */}
+            <div className='text-start mb-5'>
+                <button type="button" className='btn btn-success' onClick={handleEditClick}>Editar</button>
             </div>
 
-            {/* Contenido del lado derecho */}
-            <div className="col-sm-7" ref={editRef}>
-                <img 
-                    src={image_url} 
-                    alt="Imagen de página de inicio,"
-                    className='img-fluid rounded'
+            <div className="row text-start">
+                {/* Contenido del lado izquierdo */}
+                <div className="col-sm-5">
+                    <h2 className='fw-bold color-title mb-3'>{titulo}</h2>
+                    <p>{descripcion} </p>
+                </div>
+
+                {/* Contenido del lado derecho */}
+                <div className="col-sm-7" ref={editRef}>
+                    <img
+                        src={image_url}
+                        alt="Imagen de página de inicio,"
+                        className='img-fluid rounded'
                     />
+                </div>
+                {showEditPage && <HomePageEdit />}
             </div>
-            {showEditPage && <HomePageEdit />}
         </div>
-    </div>
 
-  )
+    )
 }
