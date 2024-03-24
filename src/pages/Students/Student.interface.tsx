bb361e608cb36ca0fc38d0940c21f95391824f43
Josué Quesada <josuequesadaxd@gmail.com>
2024-03-23 22:10:57 -0700

    updated table component

---
+++ src/pages/Students/Student.interface.tsx
@@ -0,0 +1,14 @@
+export interface Student{
+    canton:          string;
+    cedula:          string;
+    correo:          string;
+    direccion:       string;
+    distrito:        string;
+    estado:          number;
+    fechaNacimiento: Date;
+    genero:          string;
+    nombre:          string;
+    provincia:       string;
+    telefono:        string;
+    user_type:       number;
+}
