6aa9d5791a1f0addadabf24f0749263145cdc91b
David Gonzalez <davidega12@gmail.com>
2024-03-25 18:15:33 -0600

    Correo Cantonal Key

--- src/components/sent_email/SentEmail.tsx
+++ src/components/sent_email/SentEmail.tsx
@@ -2,13 +2,13 @@ import emailjs from '@emailjs/browser';
 
 export const SentEmail = ({referenciaForm}: {referenciaForm: any}) : Promise<boolean> => {
 
-    const serviceId: string  = 'service_ad6dvf4';
-    const templateId: string = 'contact_form';
-    const publicKey: string  = 'ON-22qKjZUDzY0s6N';
+    // const serviceId: string  = 'service_ad6dvf4';
+    // const templateId: string = 'contact_form';
+    // const publicKey: string  = 'ON-22qKjZUDzY0s6N';
          /*Email de Cantonal*/
-  // const serviceId: string = 'service_8ymw9gn';
-  // const templateId: string = 'contact_form';
-  // const publicKey: string = 'Z5h6nUaQ9KoLIq2kN'; 
+  const serviceId: string = 'service_8ymw9gn';
+  const templateId: string = 'contact_form';
+  const publicKey: string = 'Z5h6nUaQ9KoLIq2kN'; 
       /*Email de Cantonal*/
       
 
