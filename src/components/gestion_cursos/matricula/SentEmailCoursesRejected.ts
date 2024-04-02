22498c50ea5cb6c582adf15498f713cb039011cf
David Gonzalez <davidega12@gmail.com>
2024-04-01 23:21:03 -0600

    Email Ready using EmailJS
    I am gonna try using Resend.com for 3000 emails in the next update

--- src/components/gestion_cursos/matricula/SentEmailCoursesRejected.ts
+++ src/components/gestion_cursos/matricula/SentEmailCoursesRejected.ts
@@ -1,16 +1,14 @@
 import emailjs from '@emailjs/browser';
 
 
-export const SentEmailCoursesRejected = (nombrePersona: string, correoPersona: string, nombreCurso: string) : Promise<boolean> => {
+export const SentEmailCoursesRejected = (nombrePersona: string, correoPersona: string, nombreCurso: string): Promise<boolean> => {
 
-    // const serviceId: string  = 'service_ad6dvf4';
-    // const templateId: string = 'contact_form';
-    // const publicKey: string  = 'ON-22qKjZUDzY0s6N';
-         /*Email de Cantonal Matriculas*/
-  const serviceId: string = 'service_dkr0wxb';
-  const templateId: string = 'template_matricula_r'; // Rechazadas
-  const publicKey: string = 'd2Zek1yZFLVAozcHY'; 
-      /*Email de Cantonal MAtriculas*/  
+
+    /*Email de Cantonal Matriculas*/
+    const serviceId: string = 'service_dkr0wxb';
+    const templateId: string = 'template_matricula_r'; // Rechazadas
+    const publicKey: string = 'd2Zek1yZFLVAozcHY';
+    /*Email de Cantonal MAtriculas*/
     const information = {
         user_email: correoPersona,
         user_name: nombrePersona,
@@ -18,20 +16,20 @@ export const SentEmailCoursesRejected = (nombrePersona: string, correoPersona: s
         nombre_curso: nombreCurso,
     };
 
-        return new Promise((resolve, reject) => {
-            
-            emailjs
-                .send(serviceId, templateId, information, publicKey,
-                )
-                .then(
-                    () => {
-                        console.log('MSJ DE CORREO ENVIADO EXITOSAMENTE');
-                        resolve(true);
-                    },
-                    (error) => {
-                        console.log('FAILED... NO SE PUDO MANDAR EL MSJ DE CORREO', error.text);
-                        reject(false);
-                    },
-                );
-        })
+    return new Promise((resolve, reject) => {
+
+        emailjs
+            .send(serviceId, templateId, information, publicKey,
+            )
+            .then(
+                () => {
+                    console.log('MSJ DE CORREO ENVIADO EXITOSAMENTE');
+                    resolve(true);
+                },
+                (error) => {
+                    console.log('FAILED... NO SE PUDO MANDAR EL MSJ DE CORREO', error.text);
+                    reject(false);
+                },
+            );
+    })
 }
